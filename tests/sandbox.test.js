391d6759d76b3e135710876071399f293ec8e81c
Morior <moriorgames@gmail.com>
2018-04-02 13:19:09 +0200

    Improve project with more dummy tests

--- tests/sandbox.test.js
+++ tests/sandbox.test.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 
 var foo = 'bar';
+var fuzzy = 'amazing';
 
 describe('Executing sandbox test', () => {
     it('Foo Should be a String', () => {
@@ -11,6 +12,9 @@ describe('Executing sandbox test', () => {
     });
     it('Foo Should be length 3', () => {
         expect(foo).to.have.lengthOf(3);
+    });
+    it('A new Test Added to the project', () => {
+        expect(fuzzy).to.have.lengthOf(7);
     });
     // it('Foo Should FAIL', () => {
     //     expect(foo).to.have.lengthOf(99);
